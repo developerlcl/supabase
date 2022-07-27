dc6729371e999426668e044251186751fd3a910f
leecharleslaing <71520336+lcharleslaing@users.noreply.github.com>
2022-07-27 15:29:08 -0400

    removed rest client files

--- prisma/migrations/20220727192610_init_migrate/migration.sql
+++ prisma/migrations/20220727192610_init_migrate/migration.sql
@@ -1,9 +1,9 @@
 -- CreateTable
 CREATE TABLE "State" (
     "id" SERIAL NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
-    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+    "name" TEXT,
 
     CONSTRAINT "State_pkey" PRIMARY KEY ("id")
 );
@@ -11,9 +11,9 @@ CREATE TABLE "State" (
 -- CreateTable
 CREATE TABLE "County" (
     "id" SERIAL NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
-    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+    "name" TEXT,
     "stateId" INTEGER,
 
     CONSTRAINT "County_pkey" PRIMARY KEY ("id")
@@ -22,10 +22,10 @@ CREATE TABLE "County" (
 -- CreateTable
 CREATE TABLE "City" (
     "id" SERIAL NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
     "name" TEXT NOT NULL,
-    "countyId" INTEGER NOT NULL,
+    "countyId" INTEGER,
     "stateId" INTEGER,
 
     CONSTRAINT "City_pkey" PRIMARY KEY ("id")
@@ -38,7 +38,7 @@ CREATE UNIQUE INDEX "State_name_key" ON "State"("name");
 ALTER TABLE "County" ADD CONSTRAINT "County_stateId_fkey" FOREIGN KEY ("stateId") REFERENCES "State"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "City" ADD CONSTRAINT "City_countyId_fkey" FOREIGN KEY ("countyId") REFERENCES "County"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "City" ADD CONSTRAINT "City_countyId_fkey" FOREIGN KEY ("countyId") REFERENCES "County"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "City" ADD CONSTRAINT "City_stateId_fkey" FOREIGN KEY ("stateId") REFERENCES "State"("id") ON DELETE SET NULL ON UPDATE CASCADE;
